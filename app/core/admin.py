add0220680cecfee8e4993e67e9e0ef226016efc
SaharSheik <sahara_sheik@hotmail.com>
2022-09-07 02:32:08 -0400

    created testing for admin customization

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['first_name', 'last_name', 'email']
+
+
+admin.site.register(models.User, UserAdmin)
