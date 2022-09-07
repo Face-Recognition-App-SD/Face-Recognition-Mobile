fc03c90b358296243fc235012db40aed183c51e3
SaharSheik <sahara_sheik@hotmail.com>
2022-09-07 12:03:26 -0400

    modifyng paths- cannot get the document generator work

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,12 +23,12 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    path(
-        'api/docs/',
-        SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs',
-    )
+    # # path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    # # path(
+    # #     'api/docs/',
+    # #     SpectacularSwaggerView.as_view(url_name='api-schema'),
+    # #     name='api-docs',
+    # )
 ]
 
 # from django.contrib import admin
