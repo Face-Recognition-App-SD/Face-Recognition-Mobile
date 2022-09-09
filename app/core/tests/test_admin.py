bb6a5854e648b9b880866467588d1f90f6c06983
SaharSheik <sahara_sheik@hotmail.com>
2022-09-09 05:13:01 -0400

    testing for admin page added and admin.py class was added

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -29,5 +29,5 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
-        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.first_name)
         self.assertContains(res, self.user.email)
